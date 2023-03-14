6d5236dadc23c1da14099b5328d37e6fdaa98d25
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-14 14:02:50 +0600

    endpoint initialize

--- server/app.js
+++ server/app.js
@@ -5,6 +5,11 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 
+const productRouter = require('./routes/productRouter');
+const userRouter = require('./routes/userRouter');
+const orderRouter = require('./routes/orderRouter');
+const paymentRouter = require('./routes/paymentRouter');
+
 //  app initialize
 const app = express();
 
@@ -26,6 +31,11 @@ app.use(
 app.use(cors(corsConfig));
 app.options('*', cors(corsConfig));
 app.disable('x-powered-by'); // less hackers know about our stack
+
+app.use('/api/v1', productRouter);
+app.use('/api/v1', userRouter);
+app.use('/api/v1', orderRouter);
+app.use('/api/v1', paymentRouter);
 
 //  displaying welcome message
 app.get('/', (req, res) => {
