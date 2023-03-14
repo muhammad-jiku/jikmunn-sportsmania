cf7a2db569e4ed04434ebd774f2b9c9e86e61cfc
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-14 13:59:45 +0600

    user router

---
+++ server/routes/userRouter.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const {
+  registerUser,
+  loginUser,
+  forgotPassword,
+  resetPassword,
+  logout,
+  getUserDetails,
+  updatePassword,
+  updateProfile,
+  getAllUser,
+  getSingleUser,
+  updateUserRole,
+  deleteUser,
+} = require('../controllers/userController');
+
+const userRouter = express.Router({
+  caseSensitive: true,
+});
+
+userRouter.route('/register').post(registerUser);
+
+userRouter.route('/login').post(loginUser);
+
+userRouter.route('/password/forgot').post(forgotPassword);
+
+userRouter.route('/password/reset/:token').put(resetPassword);
+
+userRouter.route('/logout').get(logout);
+
+userRouter.route('/me').get(getUserDetails);
+
+userRouter.route('/password/update').put(updatePassword);
+
+userRouter.route('/me/update').put(updateProfile);
+
+userRouter.route('/admin/users').get(getAllUser);
+
+userRouter
+  .route('/admin/user/:id')
+  .get(getSingleUser)
+  .put(updateUserRole)
+  .delete(deleteUser);
+
+module.exports = userRouter;
