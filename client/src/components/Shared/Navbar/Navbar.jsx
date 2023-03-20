b9c638d3ee8ab74eb24f9d13aeedcf9c9692c7f9
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-20 19:15:06 +0600

    search component removed

--- client/src/components/Shared/Navbar/Navbar.jsx
+++ client/src/components/Shared/Navbar/Navbar.jsx
@@ -4,8 +4,6 @@ import {
   Toolbar,
   Typography,
   styled,
-  alpha,
-  InputBase,
   Box,
   Tooltip,
   IconButton,
@@ -17,7 +15,6 @@ import {
 import './Navbar.scss';
 import logo from '../../../assets/logo.png';
 import person1 from '../../../assets/avatar_1.png';
-import SearchIcon from '@mui/icons-material/Search';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -28,47 +25,6 @@ const settings = ['Profile', 'Account', 'Dashboard', 'Login', 'Logout'];
 const Logo = styled('img')(({ theme }) => ({
   width: '15rem',
   minWidth: '10rem',
-}));
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: '50px',
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  // minWidth:'300px',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
 }));
 
 const Navbar = () => {
@@ -94,17 +50,8 @@ const Navbar = () => {
   return (
     <AppBar position="static" className="header" color="secondary">
       <Toolbar className="toolbar">
-        {/*  Logo and Search  */}
+        {/*  Logo */}
         <Logo src={logo} sx={{ display: { xs: 'none', md: 'flex' } }} />
-        <Search sx={{ display: { xs: 'none', md: 'flex' } }}>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder="Search…"
-            inputProps={{ 'aria-label': 'search' }}
-          />
-        </Search>
 
         {/* Menus */}
         <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
