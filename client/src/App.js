388ea168251a6abb7a65d8c4bb8825e3057c8b60
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-20 21:17:58 +0600

    footer added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Shared/Footer/Footer';
 import Navbar from './components/Shared/Navbar/Navbar';
 import {
   AboutPage,
@@ -21,6 +22,7 @@ function App() {
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Shared/Footer/Footer.jsx
@@ -0,0 +1,149 @@
+import React from 'react';
+import {
+  Box,
+  Button,
+  Grid,
+  List,
+  ListItemText,
+  Stack,
+  styled,
+  TextField,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import SendIcon from '@mui/icons-material/Send';
+
+const Footer = () => {
+  const theme = useTheme();
+
+  const FooterTitle = styled(Typography)(() => ({
+    textTransform: 'uppercase',
+    marginBottom: '1em',
+  }));
+
+  const SubscribeTf = styled(TextField)(() => ({
+    '.MuiInputLabel-root': {
+      color: `${theme.palette.primary.main}`,
+    },
+
+    '.MuiInput-root::before': {
+      borderBottom: `1px solid ${theme.palette.primary.main}`,
+    },
+  }));
+  return (
+    <Box
+      sx={{
+        backgroundColor: `secondary.dark`,
+        color: 'white',
+        p: {
+          xs: 4,
+          md: 10,
+        },
+        pt: 12,
+        pb: 12,
+        fontSize: { xs: '12px', md: '14px' },
+      }}
+    >
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item md={6} lg={4}>
+          <FooterTitle variant="body1">About us</FooterTitle>
+          <Typography variant="caption2">
+            Thanks for visiting us. You can also connect with us via social
+            media.
+          </Typography>
+          <Box
+            sx={{
+              mt: 4,
+              color: `${theme.palette.primary.main}`,
+            }}
+          >
+            <FacebookIcon sx={{ mr: 1 }} />
+            <TwitterIcon sx={{ mr: 1 }} />
+            <InstagramIcon />
+          </Box>
+        </Grid>
+        <Grid item md={6} lg={2}>
+          <FooterTitle variant="body1">information</FooterTitle>
+          <List>
+            <ListItemText>
+              <Typography lineHeight={2} variant="caption2">
+                About Us
+              </Typography>
+            </ListItemText>
+            <ListItemText>
+              <Typography lineHeight={2} variant="caption2">
+                Order Tracking
+              </Typography>
+            </ListItemText>
+            <ListItemText>
+              <Typography lineHeight={2} variant="caption2">
+                Privacy &amp; Policy
+              </Typography>
+            </ListItemText>
+            <ListItemText>
+              <Typography lineHeight={2} variant="caption2">
+                Terms &amp; Conditions
+              </Typography>
+            </ListItemText>
+          </List>
+        </Grid>
+        <Grid item md={6} lg={2}>
+          <FooterTitle variant="body1">my account</FooterTitle>
+          <List>
+            <ListItemText>
+              <Typography lineHeight={2} variant="caption2">
+                Login
+              </Typography>
+            </ListItemText>
+            <ListItemText>
+              <Typography lineHeight={2} variant="caption2">
+                My Cart
+              </Typography>
+            </ListItemText>
+            <ListItemText>
+              <Typography lineHeight={2} variant="caption2">
+                My Account
+              </Typography>
+            </ListItemText>
+            <ListItemText>
+              <Typography lineHeight={2} variant="caption2">
+                Wishlist
+              </Typography>
+            </ListItemText>
+          </List>
+        </Grid>
+        <Grid item md={6} lg={4}>
+          <FooterTitle variant="body1">newsletter</FooterTitle>
+          <Stack>
+            <SubscribeTf
+              color="primary"
+              label="Email address"
+              variant="standard"
+            />
+            <Button
+              startIcon={
+                <SendIcon
+                  sx={{
+                    color: `${theme.palette.primary.main}`,
+                  }}
+                />
+              }
+              sx={{
+                mt: 4,
+                mb: 4,
+              }}
+              variant="contained"
+            >
+              Subscribe
+            </Button>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Footer;
