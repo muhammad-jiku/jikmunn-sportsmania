df08b53c0c0002bce59ae1a5c23ec0bb6e08e0f4
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-29 00:12:23 +0600

    login form

--- client/src/components/Auth/User/Login.jsx
+++ client/src/components/Auth/User/Login.jsx
@@ -1,10 +1,82 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { loginSchema } from './ValidationSchema';
 
 const Login = () => {
+  const {
+    register,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+    handleSubmit,
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+  }, [isSubmitSuccessful, reset]);
+
+  const onSubmitHandler = (values) => {
+    console.log(values);
+  };
+  console.log(errors);
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Box>
+      <Typography variant="h4" component="h1" sx={{ mb: '2rem' }}>
+        Login
+      </Typography>
+      <Box
+        component="form"
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit(onSubmitHandler)}
+      >
+        <TextField
+          sx={{ mb: 2 }}
+          label="Email"
+          fullWidth
+          required
+          type="email"
+          error={!!errors['email']}
+          helperText={errors['email'] ? errors['email'].message : ''}
+          {...register('email')}
+        />
+        <TextField
+          sx={{ mb: 2 }}
+          label="Password"
+          fullWidth
+          required
+          type="password"
+          error={!!errors['password']}
+          helperText={errors['password'] ? errors['password'].message : ''}
+          {...register('password')}
+        />
+        <TextField
+          sx={{ mb: 2 }}
+          label="Confirm Password"
+          fullWidth
+          required
+          type="password"
+          error={!!errors['passwordConfirm']}
+          helperText={
+            errors['passwordConfirm'] ? errors['passwordConfirm'].message : ''
+          }
+          {...register('passwordConfirm')}
+        />
+        <Button
+          variant="contained"
+          fullWidth
+          type="submit"
+          sx={{ py: '0.8rem', mt: '1rem' }}
+        >
+          Login
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
