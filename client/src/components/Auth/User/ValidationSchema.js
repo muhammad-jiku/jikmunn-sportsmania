efd23c740674e98725adb1e6bf8da61cae0411c5
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-29 00:07:52 +0600

    login validation schema

--- client/src/components/Auth/User/ValidationSchema.js
+++ client/src/components/Auth/User/ValidationSchema.js
@@ -15,3 +15,15 @@ export const registerSchema = object({
   path: ['passwordConfirm'],
   message: 'Passwords do not match',
 });
+
+export const loginSchema = object({
+  email: string().nonempty('Email is required').email('Invalid email'),
+  password: string()
+    .nonempty('Password is required')
+    .min(6, 'Password must be more than 8 characters')
+    .max(32, 'Password must be less than 32 characters'),
+  passwordConfirm: string().nonempty('Please confirm your password'),
+}).refine((data) => data.password === data.passwordConfirm, {
+  path: ['passwordConfirm'],
+  message: 'Passwords do not match',
+});
