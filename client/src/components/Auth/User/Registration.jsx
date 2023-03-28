e27a46da0ed3fbb46fed45056c35ef68f19a3e52
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-28 23:45:03 +0600

    registration form

--- client/src/components/Auth/User/Registration.jsx
+++ client/src/components/Auth/User/Registration.jsx
@@ -1,10 +1,124 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { registerSchema } from './ValidationSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  FormHelperText,
+  TextField,
+  Typography,
+} from '@mui/material';
 
 const Registration = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  const {
+    register,
+    formState: { errors, isSubmitSuccessful },
+    reset,
+    handleSubmit,
+  } = useForm({
+    resolver: zodResolver(registerSchema),
+  });
+
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+  }, [isSubmitSuccessful, reset]);
+
+  const onSubmitHandler = (values) => {
+    console.log(values);
+  };
+  console.log(errors);
+
   return (
-    <div>
-      <h1>Registration</h1>
-    </div>
+    <Box>
+      <Typography variant="h4" component="h1" sx={{ mb: '2rem' }}>
+        Register
+      </Typography>
+      <Box
+        component="form"
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit(onSubmitHandler)}
+      >
+        <TextField
+          sx={{ mb: 2 }}
+          label="Name"
+          fullWidth
+          required
+          error={!!errors['name']}
+          helperText={errors['name'] ? errors['name'].message : ''}
+          {...register('name')}
+        />
+        <TextField
+          sx={{ mb: 2 }}
+          label="Email"
+          fullWidth
+          required
+          type="email"
+          error={!!errors['email']}
+          helperText={errors['email'] ? errors['email'].message : ''}
+          {...register('email')}
+        />
+        <TextField
+          sx={{ mb: 2 }}
+          label="Password"
+          fullWidth
+          required
+          type="password"
+          error={!!errors['password']}
+          helperText={errors['password'] ? errors['password'].message : ''}
+          {...register('password')}
+        />
+        <TextField
+          sx={{ mb: 2 }}
+          label="Confirm Password"
+          fullWidth
+          required
+          type="password"
+          error={!!errors['passwordConfirm']}
+          helperText={
+            errors['passwordConfirm'] ? errors['passwordConfirm'].message : ''
+          }
+          {...register('passwordConfirm')}
+        />
+
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={isChecked}
+                onChange={(e) => setIsChecked(e.target.checked)}
+                required
+              />
+            }
+            {...register('terms')}
+            label={
+              <Typography color={errors['terms'] ? 'error' : 'inherit'}>
+                Accept Terms and Conditions
+              </Typography>
+            }
+          />
+          <FormHelperText error={!!errors['terms']}>
+            {errors['terms'] ? errors['terms'].message : ''}
+          </FormHelperText>
+        </FormGroup>
+
+        <Button
+          variant="contained"
+          fullWidth
+          type="submit"
+          sx={{ py: '0.8rem', mt: '1rem' }}
+        >
+          Register
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
--- client/src/components/Auth/User/ValidationSchema.js
+++ client/src/components/Auth/User/ValidationSchema.js
@@ -1,13 +1,11 @@
 import { literal, object, string } from 'zod';
 
 export const registerSchema = object({
-  name: string()
-    .nonempty('Name is required')
-    .max(32, 'Name must be less than 100 characters'),
-  email: string().nonempty('Email is required').email('Email is invalid'),
+  name: string().nonempty('Name is required').max(32, 'Name is too long'),
+  email: string().nonempty('Email is required').email('Invalid email'),
   password: string()
     .nonempty('Password is required')
-    .min(8, 'Password must be more than 8 characters')
+    .min(6, 'Password must be more than 8 characters')
     .max(32, 'Password must be less than 32 characters'),
   passwordConfirm: string().nonempty('Please confirm your password'),
   terms: literal(true, {
