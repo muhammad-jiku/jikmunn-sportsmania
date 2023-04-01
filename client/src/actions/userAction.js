c1edae358a8a0c171fbd37eb6d4c32fe881f3afe
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-01 23:54:51 +0600

    user data update action

--- client/src/actions/userAction.js
+++ client/src/actions/userAction.js
@@ -12,6 +12,9 @@ import {
   REGISTER_USER_FAIL,
   REGISTER_USER_REQUEST,
   REGISTER_USER_SUCCESS,
+  UPDATE_PROFILE_FAIL,
+  UPDATE_PROFILE_REQUEST,
+  UPDATE_PROFILE_SUCCESS,
 } from '../constants/userConstant';
 
 // Login
@@ -115,7 +118,32 @@ export const logoutUser = () => async (dispatch) => {
 };
 
 // Update Profile
-export const updateProfile = (userData) => async (dispatch) => {};
+export const updateProfile = (userData) => async (dispatch) => {
+  try {
+    dispatch({
+      type: UPDATE_PROFILE_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        authorization: `Bearer ${localStorage?.getItem('token')}`,
+        'content-type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.put(`/api/v1/me/update`, userData, config);
+
+    dispatch({
+      type: UPDATE_PROFILE_SUCCESS,
+      payload: data.success,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PROFILE_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
 // Update Password
 export const updatePassword = (passwords) => async (dispatch) => {};
