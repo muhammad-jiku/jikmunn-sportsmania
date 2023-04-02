9c0d541ffc4b67744b9e8dc93ca7701a46850298
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-02 23:11:40 +0600

    reset password action

--- client/src/actions/userAction.js
+++ client/src/actions/userAction.js
@@ -15,6 +15,9 @@ import {
   REGISTER_USER_FAIL,
   REGISTER_USER_REQUEST,
   REGISTER_USER_SUCCESS,
+  RESET_PASSWORD_FAIL,
+  RESET_PASSWORD_REQUEST,
+  RESET_PASSWORD_SUCCESS,
   UPDATE_PASSWORD_FAIL,
   UPDATE_PASSWORD_REQUEST,
   UPDATE_PASSWORD_SUCCESS,
@@ -212,7 +215,35 @@ export const forgotPassword = (email) => async (dispatch) => {
 };
 
 // Reset Password
-export const resetPassword = (token, passwords) => async (dispatch) => {};
+export const resetPassword = (token, passwords) => async (dispatch) => {
+  try {
+    dispatch({
+      type: RESET_PASSWORD_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        'content-type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.put(
+      `/api/v1/password/reset/${token}`,
+      passwords,
+      config
+    );
+
+    dispatch({
+      type: RESET_PASSWORD_SUCCESS,
+      payload: data.success,
+    });
+  } catch (error) {
+    dispatch({
+      type: RESET_PASSWORD_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
 // get All Users
 export const getAllUsers = () => async (dispatch) => {};
