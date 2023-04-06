ccdbb73352a4dbebc863c8127a49563a07b1f2c2
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-06 22:49:25 +0600

    add review reducer added

--- client/src/reducers/productReducer.js
+++ client/src/reducers/productReducer.js
@@ -12,6 +12,10 @@ import {
   ALL_REVIEW_REQUEST,
   ALL_REVIEW_SUCCESS,
   ALL_REVIEW_FAIL,
+  NEW_REVIEW_REQUEST,
+  NEW_REVIEW_SUCCESS,
+  NEW_REVIEW_FAIL,
+  NEW_REVIEW_RESET,
 } from '../constants/productConstant';
 
 export const productsReducer = (state = { products: [] }, action) => {
@@ -85,7 +89,38 @@ export const productDetailsReducer = (state = { product: {} }, action) => {
   }
 };
 
-export const newReviewReducer = (state = {}, action) => {};
+export const newReviewReducer = (state = {}, action) => {
+  switch (action.type) {
+    case NEW_REVIEW_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case NEW_REVIEW_SUCCESS:
+      return {
+        loading: false,
+        success: action.payload,
+      };
+    case NEW_REVIEW_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case NEW_REVIEW_RESET:
+      return {
+        ...state,
+        success: false,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
 
 export const productReviewsReducer = (state = { reviews: [] }, action) => {
   switch (action.type) {
