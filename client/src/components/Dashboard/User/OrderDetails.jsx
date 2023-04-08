835097f57b74ee00d1c48162d5a5ae7774091cbe
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-09 00:57:14 +0600

    dispatching order details

--- client/src/components/Dashboard/User/OrderDetails.jsx
+++ client/src/components/Dashboard/User/OrderDetails.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { clearErrors, getOrderDetails } from '../../../actions/orderAction';
+import { Loader } from '../../Shared';
 
 const OrderDetails = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { order, error, loading } = useSelector((state) => state.orderDetails);
+
+  useEffect(() => {
+    if (error) {
+      dispatch(clearErrors());
+    }
+
+    dispatch(getOrderDetails(id));
+  }, [dispatch, error, id]);
+
   return (
-    <div>
-      <h1>OrderDetails</h1>
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {console.log(order)}
+          <h1>OrderDetails</h1>
+        </>
+      )}
+    </>
   );
 };
 
