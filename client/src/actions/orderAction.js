84c41db2c5efcd9d51814f9decd1d62133574a33
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-09 00:50:01 +0600

    single order details action

--- client/src/actions/orderAction.js
+++ client/src/actions/orderAction.js
@@ -4,6 +4,9 @@ import {
   MY_ORDERS_FAIL,
   MY_ORDERS_REQUEST,
   MY_ORDERS_SUCCESS,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
 } from '../constants/orderConstant';
 
 axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN;
@@ -49,7 +52,32 @@ export const updateOrder = (id, order) => async (dispatch) => {};
 export const deleteOrder = (id) => async (dispatch) => {};
 
 // Get Order Details
-export const getOrderDetails = (id) => async (dispatch) => {};
+export const getOrderDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: ORDER_DETAILS_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        authorization: `Bearer ${localStorage?.getItem('token')}`,
+        'content-type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.get(`/api/v1/order/${id}`, config);
+
+    dispatch({
+      type: ORDER_DETAILS_SUCCESS,
+      payload: data.order,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
 // Clearing Errors
 export const clearErrors = () => async (dispatch) => {
