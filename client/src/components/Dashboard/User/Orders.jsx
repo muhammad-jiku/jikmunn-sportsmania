161954353224e7ef87abbdbcb2c8ba4c1a68bde7
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-09 00:40:21 +0600

    dispatching user orders

--- client/src/components/Dashboard/User/Orders.jsx
+++ client/src/components/Dashboard/User/Orders.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearErrors, myOrders } from '../../../actions/orderAction';
+import { Loader } from '../../Shared';
+import { Typography } from '@mui/material';
 
 const Orders = () => {
+  const dispatch = useDispatch();
+  const { loading, error, orders } = useSelector((state) => state.myOrders);
+  const { user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (error) {
+      dispatch(clearErrors());
+    }
+    dispatch(myOrders());
+  }, [dispatch, error]);
+
   return (
-    <div>
-      <h1>Orders</h1>
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {console.log(orders)}
+          <Typography>{user?.name}'s Orders</Typography>
+        </>
+      )}
+    </>
   );
 };
 
