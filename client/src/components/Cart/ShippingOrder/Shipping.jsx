2caebe27791e5c63a5c25b37a3173ba33336b377
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-09 13:39:37 +0600

    shippin component

--- client/src/components/Cart/ShippingOrder/Shipping.jsx
+++ client/src/components/Cart/ShippingOrder/Shipping.jsx
@@ -1,10 +1,150 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CheckoutSteps from './CheckoutSteps';
+import { useDispatch, useSelector } from 'react-redux';
+import { Country, State } from 'country-state-city';
+import { saveShippingInfo } from '../../../actions/cartAction';
+import { useNavigate } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import PhoneIcon from '@mui/icons-material/Phone';
+import PublicIcon from '@mui/icons-material/Public';
+import TransferWithinAStationIcon from '@mui/icons-material/TransferWithinAStation';
 
 const Shipping = () => {
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+  const { shippingInfo } = useSelector((state) => state.cart);
+
+  const [address, setAddress] = useState(shippingInfo.address);
+  const [city, setCity] = useState(shippingInfo.city);
+  const [state, setState] = useState(shippingInfo.state);
+  const [country, setCountry] = useState(shippingInfo.country);
+  const [pinCode, setPinCode] = useState(shippingInfo.pinCode);
+  const [phoneNo, setPhoneNo] = useState(shippingInfo.phoneNo);
+
+  const shippingSubmit = (e) => {
+    e.preventDefault();
+
+    if (phoneNo.length < 10 || phoneNo.length > 10) {
+      alert.error('Phone Number should be 10 digits Long');
+      return;
+    }
+    dispatch(
+      saveShippingInfo({
+        address,
+        city,
+        state,
+        country,
+        pinCode,
+        phoneNo,
+      })
+    );
+    navigate('/order/confirm');
+  };
+
   return (
-    <div>
-      <h1>Shipping</h1>
-    </div>
+    <>
+      <CheckoutSteps activeStep={0} />
+
+      <div>
+        <div>
+          <h2>Shipping Details</h2>
+
+          <form encType="multipart/form-data" onSubmit={shippingSubmit}>
+            <div>
+              <HomeIcon />
+              <input
+                type="text"
+                placeholder="Address"
+                required
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <LocationCityIcon />
+              <input
+                type="text"
+                placeholder="City"
+                required
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <FmdGoodIcon />
+              <input
+                type="number"
+                placeholder="Pin Code"
+                required
+                value={pinCode}
+                onChange={(e) => setPinCode(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <PhoneIcon />
+              <input
+                type="number"
+                placeholder="Phone Number"
+                required
+                value={phoneNo}
+                onChange={(e) => setPhoneNo(e.target.value)}
+                size="10"
+              />
+            </div>
+
+            <div>
+              <PublicIcon />
+
+              <select
+                required
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+              >
+                <option value="">Country</option>
+                {Country &&
+                  Country.getAllCountries().map((item) => (
+                    <option key={item.isoCode} value={item.isoCode}>
+                      {item.name}
+                    </option>
+                  ))}
+              </select>
+            </div>
+
+            {country && (
+              <div>
+                <TransferWithinAStationIcon />
+
+                <select
+                  required
+                  value={state}
+                  onChange={(e) => setState(e.target.value)}
+                >
+                  <option value="">State</option>
+                  {State &&
+                    State.getStatesOfCountry(country).map((item) => (
+                      <option key={item.isoCode} value={item.isoCode}>
+                        {item.name}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            )}
+
+            <input
+              type="submit"
+              value="Continue"
+              disabled={state ? false : true}
+            />
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
