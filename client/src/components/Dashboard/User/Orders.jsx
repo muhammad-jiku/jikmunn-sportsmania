6b43b51eb8413fd22ef18826cfa3587cf4195b6b
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-11 20:19:09 +0600

    order details

--- client/src/components/Dashboard/User/Orders.jsx
+++ client/src/components/Dashboard/User/Orders.jsx
@@ -2,12 +2,81 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearErrors, myOrders } from '../../../actions/orderAction';
 import { Loader } from '../../Shared';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { Link } from 'react-router-dom';
+import LaunchIcon from '@mui/icons-material/Launch';
 
 const Orders = () => {
   const dispatch = useDispatch();
   const { loading, error, orders } = useSelector((state) => state.myOrders);
   const { user } = useSelector((state) => state.user);
+
+  const columns = [
+    {
+      field: 'id',
+      headerName: 'Order ID',
+      minWidth: 300,
+      flex: 1,
+    },
+    {
+      field: 'status',
+      headerName: 'Status',
+      minWidth: 150,
+      flex: 0.5,
+      cellClassName: (params) => {
+        console.log(params.id);
+        return (params.id, 'status') === 'Delivered'
+          ? 'greenColor'
+          : 'redColor';
+      },
+    },
+    {
+      field: 'itemsQty',
+      headerName: 'Items Qty',
+      type: 'number',
+      minWidth: 150,
+      flex: 0.3,
+    },
+
+    {
+      field: 'amount',
+      headerName: 'Amount',
+      type: 'number',
+      minWidth: 270,
+      flex: 0.5,
+    },
+
+    {
+      field: 'actions',
+      flex: 0.3,
+      headerName: 'Actions',
+      minWidth: 150,
+      type: 'number',
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <>
+            {console.log(params)}
+            <Link to={`/order/${params.id}`}>
+              <LaunchIcon />
+            </Link>
+          </>
+        );
+      },
+    },
+  ];
+  const rows = [];
+
+  orders &&
+    orders.forEach((item, index) => {
+      rows.push({
+        itemsQty: item?.orderItems?.length,
+        id: item?._id,
+        status: item?.orderStatus,
+        amount: item?.totalPrice,
+      });
+    });
 
   useEffect(() => {
     if (error) {
@@ -23,7 +92,34 @@ const Orders = () => {
       ) : (
         <>
           {console.log(orders)}
-          <Typography>{user?.name}'s Orders</Typography>
+          {orders ? (
+            <>
+              <Box sx={{ height: 400, width: '100%', p: 2 }}>
+                <Typography>{user?.name}'s Orders</Typography>
+                <DataGrid
+                  rows={rows}
+                  columns={columns}
+                  initialState={{
+                    pagination: {
+                      paginationModel: {
+                        pageSize: 5,
+                      },
+                    },
+                  }}
+                  pageSizeOptions={[5]}
+                  checkboxSelection
+                  disableRowSelectionOnClick
+                />
+              </Box>
+            </>
+          ) : (
+            <Box sx={{ p: 2 }}>
+              <Typography textAlign="center" color="red">
+                {' '}
+                You did not order anything yet!{' '}
+              </Typography>{' '}
+            </Box>
+          )}
         </>
       )}
     </>
