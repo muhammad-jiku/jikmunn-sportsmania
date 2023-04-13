c69cbdcfde16acc8b45608f9026e4842805e4165
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-14 00:02:29 +0600

    all order reducer

--- client/src/reducers/orderReducer.js
+++ client/src/reducers/orderReducer.js
@@ -1,4 +1,7 @@
 import {
+  ALL_ORDERS_FAIL,
+  ALL_ORDERS_REQUEST,
+  ALL_ORDERS_SUCCESS,
   CLEAR_ERRORS,
   CREATE_ORDER_FAIL,
   CREATE_ORDER_REQUEST,
@@ -72,7 +75,34 @@ export const myOrdersReducer = (state = { orders: [] }, action) => {
 };
 
 //  - (admin)
-export const allOrdersReducer = (state = { orders: [] }, action) => {};
+export const allOrdersReducer = (state = { orders: [] }, action) => {
+  switch (action.type) {
+    case ALL_ORDERS_REQUEST:
+      return {
+        loading: true,
+      };
+
+    case ALL_ORDERS_SUCCESS:
+      return {
+        loading: false,
+        orders: action.payload,
+      };
+
+    case ALL_ORDERS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
 
 //  - (admin)
 export const orderReducer = (state = {}, action) => {};
