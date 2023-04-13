637bfcf91e748c09ae74534c151cd743c1e0ace8
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-13 21:42:04 +0600

    admin panel component

--- client/src/components/Dashboard/Admin/AdminPanel.jsx
+++ client/src/components/Dashboard/Admin/AdminPanel.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Box, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 const AdminPanel = () => {
+  const dispatch = useDispatch();
   return (
-    <div>
-      <h1>Admin Panel</h1>
-    </div>
+    <Box>
+      <Box>
+        <Typography component="h1">Dashboard</Typography>
+
+        <Box>
+          <Box>
+            <Typography variant="p">
+              Total Amount <br />
+              {/* ${totalAmount} */}
+            </Typography>
+          </Box>
+          <Box>
+            <Link to="/admin/products">
+              <Typography variant="p">Product</Typography>
+              {/* <Typography variant='p'>{products && products.length}</Typography> */}
+            </Link>
+            <Link to="/admin/orders">
+              <Typography variant="p">Orders</Typography>
+              {/* <Typography variant='p'>{orders && orders.length}</Typography> */}
+            </Link>
+            <Link to="/admin/users">
+              <Typography variant="p">Users</Typography>
+              {/* <Typography variant='p'>{users && users.length}</Typography> */}
+            </Link>
+          </Box>
+        </Box>
+
+        <Box>{/* <Line data={lineState} /> */}</Box>
+
+        <Box>{/* <Doughnut data={doughnutState} /> */}</Box>
+      </Box>
+    </Box>
   );
 };
 
