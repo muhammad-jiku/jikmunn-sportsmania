77557bf500f07009ce724b87ff43299f35cd8be3
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-14 23:03:41 +0600

    user details action by admin

--- client/src/actions/userAction.js
+++ client/src/actions/userAction.js
@@ -27,6 +27,9 @@ import {
   UPDATE_PROFILE_FAIL,
   UPDATE_PROFILE_REQUEST,
   UPDATE_PROFILE_SUCCESS,
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
 } from '../constants/userConstant';
 
 // Login
@@ -278,7 +281,32 @@ export const getAllUsers = () => async (dispatch) => {
 };
 
 // get  User Details
-export const getUserDetails = (id) => async (dispatch) => {};
+export const getUserDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        authorization: `Bearer ${localStorage?.getItem('token')}`,
+        'content-type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.get(`/api/v1/admin/user/${id}`, config);
+
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
 // Update User - (admin)
 export const updateUser = (id, userData) => async (dispatch) => {};
