4a4f01e457ed2f7e3e6ff9ba049805391ca54654
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-15 00:56:48 +0600

    user action by admin for delete and update

--- client/src/components/Dashboard/Admin/ManageUsers/UpdateUser.jsx
+++ client/src/components/Dashboard/Admin/ManageUsers/UpdateUser.jsx
@@ -1,10 +1,125 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  clearErrors,
+  getUserDetails,
+  updateUser,
+} from '../../../../actions/userAction';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { UPDATE_USER_RESET } from '../../../../constants/userConstant';
+import { Loader } from '../../../Shared';
+import PersonIcon from '@mui/icons-material/Person';
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import { Button } from '@mui/material';
 
 const UpdateUser = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+  const { loading, error, user } = useSelector((state) => state.userDetails);
+
+  const {
+    loading: updateLoading,
+    error: updateError,
+    isUpdated,
+  } = useSelector((state) => state.profile);
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [role, setRole] = useState('');
+
+  useEffect(() => {
+    if (user && user._id !== id) {
+      dispatch(getUserDetails(id));
+    } else {
+      setName(user.name);
+      setEmail(user.email);
+      setRole(user.role);
+    }
+    if (error) {
+      dispatch(clearErrors());
+    }
+
+    if (updateError) {
+      dispatch(clearErrors());
+    }
+
+    if (isUpdated) {
+      navigate('/dashboard/admin/users');
+      dispatch({
+        type: UPDATE_USER_RESET,
+      });
+    }
+  }, [dispatch, user, error, updateError, isUpdated, navigate, id]);
+
+  const updateUserSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const myForm = new FormData();
+
+    myForm.set('name', name);
+    myForm.set('email', email);
+    myForm.set('role', role);
+
+    dispatch(updateUser(id, myForm));
+  };
+
   return (
-    <div>
-      <h1>Update User</h1>
-    </div>
+    <>
+      <div>
+        <div>
+          {loading ? (
+            <Loader />
+          ) : (
+            <form onSubmit={updateUserSubmitHandler}>
+              <h1>Update User</h1>
+
+              <div>
+                <PersonIcon />
+                <input
+                  type="text"
+                  placeholder="Name"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div>
+                <MailOutlineIcon />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <VerifiedUserIcon />
+                <select value={role} onChange={(e) => setRole(e.target.value)}>
+                  <option value="">Choose Role</option>
+                  <option value="admin">Admin</option>
+                  <option value="user">User</option>
+                </select>
+              </div>
+
+              <Button
+                id="createProductBtn"
+                type="submit"
+                disabled={
+                  updateLoading ? true : false || role === '' ? true : false
+                }
+              >
+                Update
+              </Button>
+            </form>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
