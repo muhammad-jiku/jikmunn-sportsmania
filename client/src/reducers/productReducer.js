f2b22d0dac2322e2224db2277eac5b9c7eb2b398
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-15 22:03:07 +0600

    product reducer

--- client/src/reducers/productReducer.js
+++ client/src/reducers/productReducer.js
@@ -16,6 +16,14 @@ import {
   NEW_REVIEW_SUCCESS,
   NEW_REVIEW_FAIL,
   NEW_REVIEW_RESET,
+  DELETE_PRODUCT_REQUEST,
+  UPDATE_PRODUCT_REQUEST,
+  DELETE_PRODUCT_SUCCESS,
+  UPDATE_PRODUCT_SUCCESS,
+  DELETE_PRODUCT_FAIL,
+  UPDATE_PRODUCT_FAIL,
+  DELETE_PRODUCT_RESET,
+  UPDATE_PRODUCT_RESET,
 } from '../constants/productConstant';
 
 export const productsReducer = (state = { products: [] }, action) => {
@@ -61,7 +69,53 @@ export const productsReducer = (state = { products: [] }, action) => {
 export const newProductReducer = (state = { product: {} }, action) => {};
 
 // - (admin)
-export const productReducer = (state = {}, action) => {};
+export const productReducer = (state = {}, action) => {
+  switch (action.type) {
+    case DELETE_PRODUCT_REQUEST:
+    case UPDATE_PRODUCT_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case DELETE_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isDeleted: action.payload,
+      };
+
+    case UPDATE_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isUpdated: action.payload,
+      };
+    case DELETE_PRODUCT_FAIL:
+    case UPDATE_PRODUCT_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case DELETE_PRODUCT_RESET:
+      return {
+        ...state,
+        isDeleted: false,
+      };
+    case UPDATE_PRODUCT_RESET:
+      return {
+        ...state,
+        isUpdated: false,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
 
 export const productDetailsReducer = (state = { product: {} }, action) => {
   switch (action.type) {
