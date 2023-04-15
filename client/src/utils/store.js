ccaf1718eb1d72b41ea5d7ce86faf8836121e84c
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-16 00:13:30 +0600

    new product reducer stored

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -14,6 +14,7 @@ import {
   productReviewsReducer,
   newReviewReducer,
   productReducer,
+  newProductReducer,
 } from '../reducers/productReducer';
 import {
   allOrdersReducer,
@@ -32,7 +33,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   products: productsReducer,
   productDetails: productDetailsReducer,
-  // newProduct: newProductReducer,
+  newProduct: newProductReducer,
   product: productReducer,
   newReview: newReviewReducer,
   productReviews: productReviewsReducer,
