2e255c16a76c48e0523153f25b073be5c1bd049e
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-24 15:30:37 +0600

    admin panel stylize and modified

--- client/src/components/Dashboard/Admin/Summary/AdminPanel.jsx
+++ client/src/components/Dashboard/Admin/Summary/AdminPanel.jsx
@@ -30,6 +30,7 @@ Chart.register(
 
 const AdminPanel = () => {
   const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
   const { users } = useSelector((state) => state.allUsers);
   const { orders } = useSelector((state) => state.allOrders);
   const { products } = useSelector((state) => state.products);
@@ -61,7 +62,7 @@ const AdminPanel = () => {
       {
         label: 'TOTAL AMOUNT',
         backgroundColor: ['tomato'],
-        hoverBackgroundColor: ['rgb(197, 72, 49)'],
+        hoverBackgroundColor: ['rgb(236, 109, 87)'],
         data: [0, totalAmount],
       },
     ],
@@ -78,42 +79,264 @@ const AdminPanel = () => {
     ],
   };
 
-  return (
-    <Box>
-      <Box>
-        <Typography component="h1">Dashboard</Typography>
+  const options = {
+    responsive: true, // Enable responsiveness
+    maintainAspectRatio: false, // Disable aspect ratio
+  };
 
-        <Box>
-          <Box>
-            <Typography variant="p">
-              Total Amount <br />${totalAmount}
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Typography
+        variant="p"
+        sx={{
+          mt: 2,
+          fontSize: {
+            xs: '18px',
+            md: '26px',
+          },
+          fontWeight: 900,
+        }}
+      >
+        {user?.name ? user?.name + "'s" : ''} Dashboard
+      </Typography>{' '}
+      <Typography
+        variant="p"
+        sx={{
+          p: 2,
+          my: 2,
+          width: {
+            xs: '80%',
+            md: '50%',
+          },
+          color: 'whitesmoke',
+          textAlign: 'center',
+          backgroundColor: 'primary.main',
+          fontSize: {
+            xs: '14px',
+            md: '20px',
+          },
+        }}
+      >
+        Total Amount ${totalAmount}
+      </Typography>
+      {/*  Controllers */}
+      <Box
+        sx={{
+          p: 2,
+          mx: {
+            xs: 0,
+            md: 6,
+          },
+          display: 'flex',
+          // flexDirection: {
+          //   xs: 'column',
+          //   md: 'row',
+          // },
+          justifyContent: {
+            // xs: 'space-between',
+            xs: 'space-around',
+            md: 'center',
+          },
+          alignItems: 'center',
+        }}
+      >
+        {/*  Users */}
+        <Link to="/dashboard/admin/users" style={{ textDecoration: 'none' }}>
+          <Box
+            sx={{
+              m: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: {
+                xs: '90px',
+                sm: '150px',
+                md: '250px',
+              },
+              width: {
+                xs: '90px',
+                sm: '150px',
+                md: '250px',
+              },
+              color: 'whitesmoke',
+              backgroundColor: '#779203',
+              borderRadius: '50%',
+            }}
+          >
+            <Typography
+              variant="p"
+              sx={{
+                fontSize: {
+                  xs: '14px',
+                  sm: '20px',
+                  md: '26px',
+                },
+                fontWeight: 900,
+              }}
+            >
+              {users && users?.length === 1 ? 'User' : 'Users'}
+            </Typography>
+            <Typography
+              variant="p"
+              sx={{
+                fontSize: {
+                  xs: '12px',
+                  sm: '16px',
+                  md: '20px',
+                },
+                fontWeight: 700,
+              }}
+            >
+              {users && users?.length}
             </Typography>
           </Box>
-          <Box>
-            <Link to="/dashboard/admin/products">
-              <Typography variant="p">Products</Typography>
-              <Typography variant="p">
-                {products && products?.length}
-              </Typography>
-            </Link>
-            <Link to="/dashboard/admin/orders">
-              <Typography variant="p">Orders</Typography>
-              <Typography variant="p">{orders && orders?.length}</Typography>
-            </Link>
-            <Link to="/dashboard/admin/users">
-              <Typography variant="p">Users</Typography>
-              <Typography variant="p">{users && users?.length}</Typography>
-            </Link>
+        </Link>
+
+        {/*  Products */}
+        <Link to="/dashboard/admin/products" style={{ textDecoration: 'none' }}>
+          <Box
+            sx={{
+              m: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: {
+                xs: '90px',
+                sm: '150px',
+                md: '250px',
+              },
+              width: {
+                xs: '90px',
+                sm: '150px',
+                md: '250px',
+              },
+              color: 'whitesmoke',
+              backgroundColor: '#BDA305',
+              borderRadius: '50%',
+            }}
+          >
+            <Typography
+              variant="p"
+              sx={{
+                fontSize: {
+                  xs: '14px',
+                  sm: '20px',
+                  md: '26px',
+                },
+                fontWeight: 900,
+              }}
+            >
+              {products && products?.length === 1 ? 'Product' : 'Products'}
+            </Typography>
+            <Typography
+              variant="p"
+              sx={{
+                fontSize: {
+                  xs: '12px',
+                  sm: '16px',
+                  md: '20px',
+                },
+                fontWeight: 700,
+              }}
+            >
+              {products && products?.length}
+            </Typography>
           </Box>
-        </Box>
+        </Link>
 
-        <Box>
-          <Line data={lineState} />
-        </Box>
-
-        <Box>
-          <Doughnut data={doughnutState} />
-        </Box>
+        {/* Orders */}
+        <Link to="/dashboard/admin/orders" style={{ textDecoration: 'none' }}>
+          <Box
+            sx={{
+              m: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: {
+                xs: '90px',
+                sm: '150px',
+                md: '250px',
+              },
+              width: {
+                xs: '90px',
+                sm: '150px',
+                md: '250px',
+              },
+              color: 'whitesmoke',
+              backgroundColor: '#0D838E',
+              borderRadius: '50%',
+            }}
+          >
+            <Typography
+              variant="p"
+              sx={{
+                fontSize: {
+                  xs: '14px',
+                  sm: '20px',
+                  md: '26px',
+                },
+                fontWeight: 900,
+              }}
+            >
+              {orders && orders?.length === 1 ? 'Order' : 'Orders'}
+            </Typography>
+            <Typography
+              variant="p"
+              sx={{
+                fontSize: {
+                  xs: '12px',
+                  sm: '16px',
+                  md: '20px',
+                },
+                fontWeight: 700,
+              }}
+            >
+              {orders && orders?.length}
+            </Typography>
+          </Box>
+        </Link>
+      </Box>
+      {/*  Line Chart */}
+      <Box
+        sx={{
+          px: 1,
+          py: 2,
+          // boxSizing: 'border-box',
+          margin: 'auto',
+          width: '100%',
+          height: {
+            xs: 300,
+            md: 400,
+          },
+        }}
+      >
+        <Line data={lineState} options={options} />
+      </Box>
+      {/*  Doughnut Chart */}
+      <Box
+        sx={{
+          px: 1,
+          py: 2,
+          // boxSizing: 'border-box',
+          margin: 'auto',
+          width: '100%',
+          height: {
+            xs: 300,
+            md: 400,
+          },
+        }}
+      >
+        <Doughnut data={doughnutState} options={options} />
       </Box>
     </Box>
   );
