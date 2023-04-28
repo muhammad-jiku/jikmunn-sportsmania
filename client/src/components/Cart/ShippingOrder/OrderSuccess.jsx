65e6d0edffb3f4e1623102e14c19db55d8d6c685
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-28 21:12:37 +0600

    order succes message page modified

--- client/src/components/Cart/ShippingOrder/OrderSuccess.jsx
+++ client/src/components/Cart/ShippingOrder/OrderSuccess.jsx
@@ -5,13 +5,44 @@ import { Link } from 'react-router-dom';
 
 const OrderSuccess = () => {
   return (
-    <>
-      <Box>
-        <CheckCircleIcon />
-        <Typography>Your Order has been Placed successfully </Typography>
-        <Link to="/dashboard/myorders">View Orders</Link>
+    <Box
+      sx={{
+        p: 2,
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <CheckCircleIcon sx={{ mr: 1 }} />
+        <Typography
+          sx={{
+            fontSize: {
+              xs: '16px',
+              md: '20px',
+            },
+          }}
+        >
+          Your Order has been Placed successfully{' '}
+        </Typography>
       </Box>
-    </>
+      <Box sx={{ p: 2, fontSize: '20px' }}>
+        <Link
+          to="/dashboard/myorders"
+          style={{ color: '#682404', textDecoration: 'none' }}
+        >
+          View Orders
+        </Link>
+      </Box>
+    </Box>
   );
 };
 
