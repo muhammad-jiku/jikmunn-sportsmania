81f4c00c0bf3c0de7fe577f92037eb74a3ca8ed1
muhammad-jiku <muhammadjiku364@gmail.com>
2023-04-28 12:13:29 +0600

    checkout steps modified

--- client/src/components/Cart/ShippingOrder/CheckoutSteps.jsx
+++ client/src/components/Cart/ShippingOrder/CheckoutSteps.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Stepper, Step, StepLabel, Typography, useTheme } from '@mui/material';
+import {
+  Box,
+  Stepper,
+  Step,
+  StepLabel,
+  Typography,
+  useTheme,
+} from '@mui/material';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import LibraryAddCheckIcon from '@mui/icons-material/LibraryAddCheck';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
@@ -8,41 +15,79 @@ const CheckoutSteps = ({ activeStep }) => {
   const theme = useTheme();
   const steps = [
     {
-      label: <Typography>Shipping Details</Typography>,
+      label: (
+        <Typography
+          sx={{
+            fontSize: {
+              xs: '11px',
+              sm: '14px',
+              md: '16px',
+            },
+          }}
+        >
+          Shipping Details
+        </Typography>
+      ),
       icon: <LocalShippingIcon />,
     },
     {
-      label: <Typography>Confirm Order</Typography>,
+      label: (
+        <Typography
+          sx={{
+            fontSize: {
+              xs: '11px',
+              sm: '14px',
+              md: '16px',
+            },
+          }}
+        >
+          Confirm Order
+        </Typography>
+      ),
       icon: <LibraryAddCheckIcon />,
     },
     {
-      label: <Typography>Payment</Typography>,
+      label: (
+        <Typography
+          sx={{
+            fontSize: {
+              xs: '11px',
+              sm: '14px',
+              md: '16px',
+            },
+          }}
+        >
+          Payment
+        </Typography>
+      ),
       icon: <AccountBalanceIcon />,
     },
   ];
 
   return (
-    <Stepper activeStep={activeStep}>
-      {steps.map((item, index) => (
-        <Step
-          key={index}
-          active={activeStep === index ? true : false}
-          completed={activeStep >= index ? true : false}
-        >
-          <StepLabel
-            style={{
-              color:
-                activeStep >= index
-                  ? `${theme.palette.primary.main}`
-                  : 'rgba(0, 0, 0, 0.649)',
-            }}
-            icon={item.icon}
+    <Box sx={{ p: 2 }}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((item, index) => (
+          <Step
+            key={index}
+            active={activeStep === index ? true : false}
+            completed={activeStep >= index ? true : false}
           >
-            {item.label}
-          </StepLabel>
-        </Step>
-      ))}
-    </Stepper>
+            <StepLabel
+              style={{
+                color:
+                  activeStep >= index
+                    ? `${theme.palette.primary.main}`
+                    : 'rgba(0, 0, 0, 0.649)',
+              }}
+              icon={item?.icon}
+            >
+              {item?.label}
+            </StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
   );
 };
 
